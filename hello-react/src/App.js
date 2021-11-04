4f7c00fbea22673e4f808c9b64c74f0d79210b77
PAROMA <dltkddns833@naver.com>
2021-11-04 10:59:18 +0900

    JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: '#ffffff'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name} 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+      <h2>{name === '리액트' && '리액트 입니다.'}</h2>
+      <h2>{name || 'undefined입니다.'}</h2>
+      <div style={style}>{name} inline Style</div>
+      <div className="react">{name} Class Style</div>
+      <input></input>
+    </>
   );
 }
 
