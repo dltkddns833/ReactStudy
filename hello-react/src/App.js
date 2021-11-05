3df8177a4eab51762df07d3597f4b3c118361b2f
PAROMA <dltkddns833@naver.com>
2021-11-05 11:03:57 +0900

    LifeCycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,14 +6,43 @@ import EventPractice from "./components/EventPractice";
 import ValidSample from "./components/VaildSample";
 import ScrollBox from "./components/ScrollBox";
 import IterationSample from "./components/IterationSample";
+import { Component } from "react/cjs/react.production.min";
+import LifeCycleSample from "./components/LifeCycleSample";
 
-const App = () => {
-  const scrollBox = useRef();
-  return (
-    <div>
-      <IterationSample/>
-    </div>
-  )
+// // 함수형
+// const App = () => {
+//   const scrollBox = useRef();
+//   return (
+//     <div>
+//       <IterationSample/>
+//     </div>
+//   )
+// }
+
+// 클래스형
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component{
+  state = {
+    color : '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color : getRandomColor()
+    })
+  }
+
+  render(){
+    return(
+      <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color}/>
+      </div>
+    )
+  }
 }
 
 export default App;
