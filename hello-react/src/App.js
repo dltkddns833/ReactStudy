e44064b2655ef307c48b0ac8a183fd7ff3010bdf
PAROMA <dltkddns833@naver.com>
2021-11-05 09:57:20 +0900

    ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,13 @@
+import React, { useState, useRef } from "react";
 import MyComponent from "./components/MyComponent";
 import Counter from "./components/Counter";
 import Say from "./components/Say";
 import EventPractice from "./components/EventPractice";
+import ValidSample from "./components/VaildSample";
+import ScrollBox from "./components/ScrollBox";
 
 const App = () => {
+  const scrollBox = useRef();
   return (
     <div>
       <MyComponent name={"react"} favoriteNumber={3}>리액트</MyComponent>
@@ -13,6 +17,13 @@ const App = () => {
       <Say/>
       <br/>
       <EventPractice/>
+      <br/>
+      <ValidSample/>
+      <br/>
+      <div>
+        <ScrollBox ref={scrollBox}/>
+        <button onClick={() => scrollBox.current.scrollToBottom()}>맨 밑으로</button>
+      </div>
     </div>
   )
 }
