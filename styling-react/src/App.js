dc620678f332f8861ebeca37654380f197b029f4
Sang Wun <dltkddns833@naver.com>
2021-11-06 13:34:38 +0900

    Commit before yarn eject

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import SassComponent from "./components/SassComponent";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <SassComponent/>
+      </div>
+    )
+  }
+}
+export default App;
