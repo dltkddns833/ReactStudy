535c0d8f095c8515e7899eebba936fe20f72e588
PAROMA <dltkddns833@naver.com>
2021-11-12 17:55:54 +0900

    react-redux

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter";
+import Todos from "./components/Todos";
+
+const App = () => {
+  return (
+    <div>
+      <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
