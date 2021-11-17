c7c09b45d8c90cb293d6ea2ecc52ba0af306cf8d
PAROMA <dltkddns833@naver.com>
2021-11-17 14:26:07 +0900

    서버 사이드 렌더링

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,17 +1,21 @@
-import { Route } from "react-router-dom";
-import Menu from "./components/Menu";
-import RedPage from "./pages/RedPage";
-import BluePage from "./pages/BluePage";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import loadable from '@loadable/component';
+const RedPage = loadable(() => import('./pages/RedPage'));
+const BluePage = loadable(() => import('./pages/BluePage'));
+const UsersPage = loadable(() => import('./pages/UsersPage'));
 
 const App = () => {
   return (
     <div>
-      <Menu/>
-      <hr/>
-      <Route path="/red" component={RedPage}/>
-      <Route path="/blue" component={BluePage}/>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- ssr-recipe/src/components/Menu.js
+++ ssr-recipe/src/components/Menu.js
@@ -8,6 +8,9 @@ const Menu = () => {
             <li>
                 <Link to="/blue">Blue</Link>
             </li>
+            <li>
+                <Link to="/users">Users</Link>
+            </li>
         </ul>
     )
 }
