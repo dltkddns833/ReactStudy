0592bd084121e882aeb907a86ad5f243cbd91221
PAROMA <dltkddns833@naver.com>
2021-11-17 10:29:05 +0900

    Commit before eject

---
+++ ssr-recipe/src/App.js
@@ -0,0 +1,17 @@
+import { Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+const App = () => {
+  return (
+    <div>
+      <Menu/>
+      <hr/>
+      <Route path="/red" component={RedPage}/>
+      <Route path="/blue" component={BluePage}/>
+    </div>
+  )
+}
+
+export default App;
