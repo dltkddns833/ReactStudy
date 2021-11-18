a9653a1d07fc46bb310b4e9b414840540c2f15d2
Sang Wun <dltkddns833@naver.com>
2021-11-18 17:34:30 +0900

    mongoDB

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
+
+const { PORT } = process.env;
 
 const api = require("./api");
 
@@ -13,6 +16,8 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log("Listening to port 4000");
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Listening to port %d", port);
 });
